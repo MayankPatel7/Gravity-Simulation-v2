6255e22cea0e9920b009b27b691584a967239014
Mayank Patel <91795753+MayankPatel7@users.noreply.github.com>
2026-06-14 12:10:18 +0530

    Velocity can be changed in runtime with arrow keys, added displays for orbital stats, Time scale can be changed in runtime

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import spacecraft
 import planet
+import math
 from sys import exit
 
 pygame.init()
@@ -14,8 +15,10 @@ clock = pygame.time.Clock()
 FPS = 100
 TIME_SCALE = 1
 
-craft = spacecraft.Spacecraft(1, 340, 360, 0, -900)
-earth = planet.Planet(50000, 440, 360, 64, "earth.png")
+stats_font = pygame.font.Font(None, 32)
+
+craft = spacecraft.Spacecraft(1, 640, 160, 500, 0)
+earth = planet.Planet(50000, 640, 360, 64, "earth.png")
 
 running = True
 while(running):
@@ -24,17 +27,35 @@ while(running):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PERIOD:
+                TIME_SCALE += 0.5
+            if event.key == pygame.K_COMMA:
+                TIME_SCALE -= 0.5
+            if(event.key == pygame.K_o):
+                craft.orbital_vel(earth)
+            if event.key == pygame.K_UP: craft.velY -= 5
+            elif event.key == pygame.K_DOWN: craft.velY += 5
+
+            if event.key == pygame.K_RIGHT: craft.velX += 5
+            elif event.key == pygame.K_LEFT: craft.velX -= 5
 
     screen.blit(earth.surface, earth.pos)
     earth.update()
 
     for i, point in enumerate(craft.trail):
-        alpha = int(255 * i / len(craft.trail))  # fade older points
-        pygame.draw.circle(screen, (255, 255, alpha), point, 1)
+        alpha = int(255 * i / len(craft.trail))
+        pygame.draw.circle(screen, (alpha, 255, 255), point, 1)
 
     craft.gravity(earth)
     craft.update(dt)
     screen.blit(craft.surface, craft.pos)
+
+    stats = f"Apogee: {str(craft.apogee)}\nPerigee: {str(craft.perigee)}\nAltitude: {str(craft.altitude)}\nVelocity: {str(int(math.sqrt(craft.velX**2 + craft.velY**2)))} px/s"
+    stats_text_surface = stats_font.render(stats, True, (255, 255, 255))
+    timescale_text_surface = stats_font.render(f"x{str(TIME_SCALE)}", True, (255, 255, 255))
+    screen.blit(stats_text_surface, (10, 10))
+    screen.blit(timescale_text_surface, (630, 10))
 
     pygame.display.update()
 pygame.quit()
--- spacecraft.py
+++ spacecraft.py
@@ -8,16 +8,32 @@ class Spacecraft(pygame.sprite.Sprite):
         super().__init__()
         self.surface = pygame.Surface((15, 15))
         self.surface.fill((33, 255, 248))
+
         self.pos = self.surface.get_rect(center = (x, y))
         self.velX = velX
         self.velY = velY
         self.accX = 0
         self.accY = 0
         self.mass = mass
-        self.x = float(x)  # float position
+        self.x = float(x)
         self.y = float(y)
+
         self.trail = []
         self.trail_length = 500
+
+        self.perigee = float('inf')
+        self.apogee = 0
+        self.altitude = 0
+
+    def orbital_vel(self, planet):
+        self.velX = 0
+        self.velY = 0
+        dx = planet.pos.centerx - self.pos.centerx
+        dy = planet.pos.centery - self.pos.centery
+        r = math.sqrt((dx**2 + dy**2))
+        req_vel = math.sqrt((1000*planet.mass)/r)
+        self.velX = req_vel * (dy/r)
+        self.velY = req_vel * (-dx/r)
 
     def gravity(self, planet):
         dx = planet.pos.centerx - self.pos.centerx
@@ -27,17 +43,23 @@ class Spacecraft(pygame.sprite.Sprite):
         self.accX += (force/self.mass)*(dx/r)
         self.accY += (force/self.mass)*(dy/r)
 
+        self.altitude = int(r-planet.radius)
+        if r >= self.apogee:
+            self.apogee = int(r)
+        if r <= self.perigee:
+            self.perigee = int(r)
+
     def update(self, dt):
         self.velX += self.accX * dt
         self.velY += self.accY * dt
-        self.x += self.velX * dt  # update floats
+        self.x += self.velX * dt
         self.y += self.velY * dt
-        self.pos.centerx = int(self.x)  # only convert to int for drawing
+        self.pos.centerx = int(self.x)
         self.pos.centery = int(self.y)
 
         self.trail.append((self.pos.centerx, self.pos.centery))
-        # if len(self.trail) > self.trail_length:
-        #     self.trail.pop(0)
+        if len(self.trail) > self.trail_length:
+            self.trail.pop(0)
 
         self.accY = 0
         self.accX = 0
